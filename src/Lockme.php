a604d97d82e88120d9a66000e37eda4f71f9c9a4
Jakub Caban <kuba@whyblack.pl>
2017-11-15 16:14:36 +0100

    Migrate to OAuth2

--- src/Lockme.php
+++ src/Lockme.php
@@ -1,40 +1,131 @@
 <?php
-namespace Lockme;
+namespace Lockme\SDK;
+use League\OAuth2\Client\Token\AccessToken;
+use Lockme\OAuth2\Client\Provider\Lockme;
 
+/**
+ * Lockme SDK object
+ */
 class Lockme{
-  const API_URL="https://lockme.pl/api/";
-  const API_VERSION = "1.0";
+  /**
+   * Lockme OAuth2 provider
+   * @var Lockme
+   */
+  private $provider = null;
+  /**
+   * Default access token
+   * @var AccessToken
+   */
+  private $accessToken = null;
 
-  private $key;
-  private $secret;
-
-  function __construct($key, $secret){
-    $this->key = $key;
-    $this->secret = $secret;
+  /**
+   * Object constructor
+   * @param array $options  Options for Lockme Provider
+   */
+  function __construct(array $options=[]){
+    $this->provider = new Lockme($options);
   }
 
-  public function Decrypt(){
-    $body = file_get_contents("php://input");
-    $hash = sha1($body.$this->secret);
-    if($hash !== $_SERVER['HTTP_SIGNATURE']){
-      throw new LockMe_Exception("Wrong signature");
+  /**
+   * Generate authorization URL
+   * @param string $redirectUri Redirect URI
+   * @param  array  $scopes Array of requested scopes
+   * @return string         Redirect URL
+   */
+  public function getAuthorizationUrl($redirectUri, $scopes = []){
+    $this->provider;
+    $authorizationUrl = $this->provider->getAuthorizationUrl([
+      'scope' => $scope,
+      'redirectUri' => $redirectUri
+    ]);
+    $_SESSION['oauth2_lockme_state'] = $this->provider->getState();
+    return $authorizationUrl;
+  }
+
+  /**
+   * Get Access token from AuthCode
+   * @param  string $code AuthCode
+   * @param  string $state State code
+   * @return AccessToken       Access Token
+   */
+  public function getTokenForCode($code, $state){
+    if($state != $_SESSION['oauth2_lockme_state']){
+      unset($_SESSION['oauth2_lockme_state']);
+      throw new \Exception("Wrong state");
     }
-    return json_decode($body, true);
+    unset($_SESSION['oauth2_lockme_state']);
+
+    $this->accessToken = $this->provider->getAccessToken('authorization_code', [
+        'code' => $code
+    ]);
+    return $this->accessToken;
   }
 
-  public function Test(){
-    return $this->_request("test");
+  /**
+   * Refresh access token
+   * @param  AccessToken|null $accessToken Access token
+   * @return AccessToken        Refreshed token
+   */
+  public function refreshToken($accessToken = null){
+    $accessToken = $accessToken ?: $this->accessToken;
+    $this->accessToken = $provider->getAccessToken('refresh_token', [
+      'refresh_token' => $accessToken->getRefreshToken()
+    ]);
+    return $this->accessToken;
   }
 
-  public function RoomList(){
-    return $this->_request("rooms");
+  /**
+   * Create default access token
+   * @param string|AccessToken $token Default access token
+   * @return AccessToken
+   */
+  public function setDefaultAccessToken($token){
+    if(is_string($token)){
+      $this->accessToken = new AccessToken(json_decode($token, true));
+    }else if($token instanceof AccessToken){
+      $this->accessToken = $token;
+    }else{
+      throw new \Exception("Incorrect access token");
+    }
+    return $this->accessToken;
   }
 
-  public function Reservation($id){
-    return $this->_request("reservation/{$id}");
+  /**
+   * Send message to /test endpoint
+   * @param string|AccessToken|null $accessToken Access token
+   * @return string
+   */
+  public function Test($accessToken = null){
+    return $this->provider->executeRequest("GET", "/test", $accessToken ?: $this->accessToken);
   }
 
-  public function AddReservation($data){
+  /**
+   * Get list of available rooms
+   * @param string|AccessToken|null $accessToken Access token
+   * @return array
+   */
+  public function RoomList($accessToken = null){
+    return $this->provider->executeRequest("GET", "/rooms", $accessToken ?: $this->accessToken);
+  }
+
+  /**
+   * Get reservation data
+   * @param int $roomId Room ID
+   * @param string $id Reservation ID
+   * @param string|AccessToken|null $accessToken Access token
+   * @return array
+   */
+  public function Reservation($roomId, $id, $accessToken = null){
+    return $this->provider->executeRequest("GET", "/room/{$roomId}/reservation/{$id}", $accessToken ?: $this->accessToken);
+  }
+
+  /**
+   * Add new reservation
+   * @param array $data        Reservation data
+   * @param string|AccessToken|null $accessToken Access token
+   * @return int
+   */
+  public function AddReservation($data, $accessToken = null){
     if(!$data['roomid']){
       throw new \Exception("No room ID");
     }
@@ -44,56 +135,29 @@ class Lockme{
     if(!$data["hour"]){
       throw new \Exception("No hour");
     }
-    return $this->_request("reservation", 'PUT', $data);
+    return $provider->executeRequest("PUT", "/room/{$data['roomid']}/reservation", $accessToken ?: $this->accessToken, $data);
   }
 
-  public function DeleteReservation($id, $data = array()){
-    return $this->_request("reservation/{$id}", 'DELETE', $data);
+  /**
+   * Delete reservation
+   * @param int $roomId Room ID
+   * @param string $id Reservation ID
+   * @param string|AccessToken|null $accessToken Access token
+   * @return bool
+   */
+  public function DeleteReservation($roomId, $id, $accessToken = null){
+    return $this->provider->executeRequest("DELETE", "/room/{$roomId}/reservation/{$id}", $accessToken ?: $this->accessToken);
   }
 
-  public function EditReservation($id, $data){
-    return $this->_request("reservation/{$id}", 'POST', $data);
-  }
-
-  public function PricerList(){
-    return $this->_request("pricers");
-  }
-
-  private function _request($url, $method='GET', $params=array()){
-    $params = json_encode($params);
-    $hash = sha1($method.$url.$params.$this->secret);
-
-    $ch = curl_init(self::API_URL."/v".self::API_VERSION."/".$url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      "Content-Type: application/json",
-      "Partner-Key: {$this->key}",
-      "Signature: {$hash}"
-    ));
-    switch($method){
-      case 'GET':
-      break;
-      case 'PUT':
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-      curl_setopt($ch, CURLOPT_POSTFIELDS,$params);
-      break;
-      case 'POST':
-      curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_POSTFIELDS,$params);
-      break;
-      case 'DELETE':
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-      curl_setopt($ch, CURLOPT_POSTFIELDS,$params);
-      break;
-    }
-    $ret = curl_exec($ch);
-    $ret = json_decode($ret, true);
-    if(is_array($ret) && array_key_exists('error', $ret)){
-      throw new LockMe_Exception($ret['error']);
-    }
-    return $ret;
+  /**
+   * Edit reservation
+   * @param int $roomId Room ID
+   * @param string $id Reservation ID
+   * @param array $data        Reservation data
+   * @param string|AccessToken|null $accessToken Access token
+   * @return bool
+   */
+  public function EditReservation($roomId, $id, $data, $accessToken = null){
+    return $provider->executeRequest("PUT", "/room/{$$roomId}/reservation/{$id}", $accessToken ?: $this->accessToken, $data);
   }
 }
-class LockMe_Exception extends \Exception{}
